822b1eab9b48fbfdc69102964ef37f0b115c77c8
Naveed69 <naveed.desai.69@gmail.com>
2024-07-28 12:40:51 +0530

    verion 1.0.1 Age Calculator

--- Project-02/index.js
+++ Project-02/index.js
@@ -5,9 +5,11 @@ const resultE1=document.getElementById("result");
 function tipCalculate(){
     if(bill.value<=0)
         alert("Please Enter valid Amount");
+    else{
     let res=bill.value*(1+tip.value/100);
 
     resultE1.innerText=`${res.toFixed(2)}$`;
+    }
 }
 
 btnE1.addEventListener("click", tipCalculate);
