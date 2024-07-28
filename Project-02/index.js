77d6255a3114f413cc577cc2f190a99753977c67
Naveed69 <naveed.desai.69@gmail.com>
2024-07-28 12:20:21 +0530

    Version 1.0.1

--- Project-02/index.js
+++ Project-02/index.js
@@ -3,6 +3,8 @@ const bill = document.getElementById("bill");
 const tip = document.getElementById("tipPercentage");
 const resultE1=document.getElementById("result");
 function tipCalculate(){
+    if(bill.value<=0)
+        alert("Please Enter valid Amount");
     let res=bill.value*(1+tip.value/100);
 
     resultE1.innerText=`${res.toFixed(2)}$`;
