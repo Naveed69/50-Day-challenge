d8249d4542b23bc1dd8fa69d0137cbe94a5f2078
Naveed69 <naveed.desai.69@gmail.com>
2024-08-01 15:23:33 +0530

    Version 1.0.0

---
+++ Project-05/index.js
@@ -0,0 +1,41 @@
+const startBtn=document.getElementById("start");
+const stopBtn=document.getElementById("stop");
+const resetBtn=document.getElementById("reset");
+let timerLable=document.getElementById("timer");
+let interval;
+let timeleft=15; 
+
+function updateTime(){
+    let min=Math.floor(timeleft/60);
+    let seconds=timeleft%60;
+    let formattedTime = `${min.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+    timerLable.innerHTML = formattedTime;
+}
+
+function startFn(){
+    interval= setInterval(()=>{
+        timeleft--;
+        updateTime();
+        if(timeleft==0){
+            clearInterval(interval);
+                alert("Time's Up...!!!");
+                timeleft=15;
+        }
+    },1000
+    )
+    // timerLable.innerHTML=Date();
+}
+
+function stopFn(){
+    clearInterval(interval);
+}
+
+function resetFn(){
+    clearInterval(interval);
+    timeleft=15;
+    updateTime();
+}
+
+startBtn.addEventListener("click", startFn);
+stopBtn.addEventListener("click", stopFn);
+resetBtn.addEventListener("click", resetFn);
