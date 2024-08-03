17d01117aa364c68a4b52d2b91fe84a23741ff1c
Naveed69 <naveed.desai.69@gmail.com>
2024-08-03 23:13:49 +0530

    HTML CSS amd JS

---
+++ Project-06/index.js
@@ -0,0 +1,40 @@
+const buttons=document.querySelectorAll("button");
+const res=document.getElementById("result");
+const ps=document.getElementById("humanP");
+const cs=document.getElementById("computerP");
+
+let playerScore=0;
+let computerScore=0;
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", ()=>
+    {
+        const result=playRound(button.id,computerPlay());
+        console.log(result);
+        res.textContent=result;
+    });
+});
+
+function computerPlay(){
+    const choices=["rock","papper","scissor"];
+    const rchoice=Math.floor(Math.random()*choices.length);
+    return choices[rchoice];
+}
+
+function playRound(pSuggestion,cSuggestion){
+    if(pSuggestion===cSuggestion)
+        return "It's a tie!!";
+    else if(
+        pSuggestion === "rock" && cSuggestion === "scissor" ||
+        pSuggestion === "scissor" && cSuggestion === "papper" ||
+        pSuggestion === "papper" && cSuggestion === "rock"
+        ){
+            playerScore++;
+            ps.textContent=playerScore;
+            return "You Win!";
+        }else {
+            computerScore++;
+            cs.textContent=computerScore;
+            return "You lose!";
+        }
+}
