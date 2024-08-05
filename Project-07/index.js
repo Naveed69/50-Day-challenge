ae34fbf53d1cf9483939f9ba58b99b9acb66219d
Naveed69 <naveed.desai.69@gmail.com>
2024-08-05 23:14:33 +0530

    html and css version 1.0.0

---
+++ Project-07/index.js
@@ -0,0 +1,22 @@
+const start=document.getElementById('start');
+const stopt=document.getElementById('stop');
+const reset=document.getElementById('reset');
+
+let time=0;
+
+function startTimer(){
+
+}
+
+function stopTimer(){
+
+}
+
+function resetTimer(){
+
+}
+
+
+start.addEeventListner('click', startTimer);
+stopt.addEeventListner('click',stopTimer);
+reset.addEventListener('click',resetTimer);
