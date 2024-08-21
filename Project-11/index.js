39f8da01a9e092d5dd8789cb337215ff311d9d08
Naveed69 <naveed.desai.69@gmail.com>
2024-08-21 21:33:46 +0530

    fetching API

--- Project-11/index.js
+++ Project-11/index.js
@@ -1,5 +1,8 @@
 const v=fetch('https://api.kanye.rest/');
 
-v.then((data)=>{
-  
-})
+v.then(d=>{
+  const vs=d.json();
+  vs.then(e=>{
+    console.log(e).catch(error=>console.log(error));
+  });
+});
