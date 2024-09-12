510e4760543985bcc16b42c1619aa87e6177d79b
Naveed69 <naveed.desai.69@gmail.com>
2024-09-12 13:13:25 +0530

    Modified JS

--- Project-13/index.js
+++ Project-13/index.js
@@ -1,10 +1,3 @@
 const nameEle=document.querySelector("#name-input");
 const emailEle=document.querySelector("#email-input");
 
-
-nameEle.addEventListener("onChange",()=>{
-    console.log(nameEle.value);
-})
-
-console.log(nameEle);
-console.log(emailEle);
