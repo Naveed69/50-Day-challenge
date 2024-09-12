0b67468c6c338ad2cca7881315c60c324d67ca26
Naveed69 <naveed.desai.69@gmail.com>
2024-09-12 13:30:15 +0530

    Modified JS

--- Project-13/index.js
+++ Project-13/index.js
@@ -1,3 +1,6 @@
 const nameEle=document.querySelector("#name-input");
 const emailEle=document.querySelector("#email-input");
 
+nameEle.addEventListener("blur",()=>{
+    console.log(nameEle.value);
+})
