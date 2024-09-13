cfd029f59b2cf19b7f4db2068e57958fd2f92c3f
Naveed69 <naveed.desai.69@gmail.com>
2024-09-13 20:07:49 +0530

    Modified JS

--- Project-13/index.js
+++ Project-13/index.js
@@ -3,12 +3,17 @@ const formEle=document.getElementById("booking-form");
 const nameEle=formEle.elements["name-input"];
 const emailEle=formEle.elements["email-input"];
 
+const nameValidation=document.getElementById("name-validation");
+
 nameEle.addEventListener("focus",()=>{
     console.log("focus"); 
+    nameValidation.innerText="";
+
 })
 
 nameEle.addEventListener("blur",()=>{
     console.log("blur");
+    nameValidation.innerText="Blured";
 })
 
 console.log("Hello");
