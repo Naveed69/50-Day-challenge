2dc922da1e126d0ca5413965a0ca86ddc5b23505
Naveed69 <naveed.desai.69@gmail.com>
2024-09-28 19:33:18 +0530

    Added some style

--- Project-13/index.js
+++ Project-13/index.js
@@ -115,8 +115,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     const stringPaload=localStorage.getItem("formData");
     const payload=JSON.parse(stringPaload);
     const {name,email}=payload;
-    //finallycompleted
-
     nameEle.value=name;
     emailEle.value=email;
 
