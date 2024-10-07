4a6dbf76765d94e3c2fe11adcc6334a224923561
Naveed69 <naveed.desai.69@gmail.com>
2024-10-07 14:58:14 +0530

    Added some js

--- Project-14/index.js
+++ Project-14/index.js
@@ -1,14 +1,36 @@
-function Car(color, brand, fuelType) {
-    this.color = color;
-    this.brand = brand;
-    this.fuelType = fuelType;
-    this.engineStart = function () {
-      console.log(`Car ${this.color} ${this.brand} started`);
-    };
-    return engineStart;
-  }
+// function Car(color, brand, fuelType) {
+//     this.color = color;
+//     this.brand = brand;
+//     this.fuelType = fuelType;
+//     this.engineStart = function () {
+//       console.log(`Car ${this.color} ${this.brand} started`);
+//     };
+//     return engineStart;
+//   }
 
-  const bmwObj = Car("black", "BMW", "Petrol");
-  console.log(bmwObj);
-  const bmwObj1 =new Car("black", "BMW", "Petrol");
-  console.log(bmwObj1);
+//   const bmwObj = Car("black", "BMW", "Petrol");
+//   console.log(bmwObj);
+//   const bmwObj1 =new Car("black", "BMW", "Petrol");
+//   console.log(bmwObj1);
+
+// console.log(Car("black", "BMW", "Petrol").color);
+
+// class Car{
+//     constructor(color,brand,fuelType){
+//         this.color = color;
+//         this.brand = brand;
+//         this.fuelType = fuelType;
+//     }
+// }
+
+// console.log(Car("black", "BMW", "Petrol").color);
+
+
+// const formatSpecified = new Date().toLocaleDateString('en-us', { day:"numeric", month:"numeric", year:"numeric"  })
+// // console.log(date);
+// console.log(formatSpecified);
+
+let res="hello"
+let re="hi";
+res=res+re;
+console.log(res);
