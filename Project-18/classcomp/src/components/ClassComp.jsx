6213454a286208491cf28231c45ad8a38492f993
naveed69 <naveed.desai.69@gmail.com>
2025-04-10 17:12:12 +0530

    completed

---
+++ Project-18/classcomp/src/components/ClassComp.jsx
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+export class ClassComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  handleincrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button type="button" onClick={this.handleincrement}>
+          Increment
+        </button>
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement
+        </button>
+      </>
+    );
+  }
+}
