f29ceaeb1a42cb99fcffab8a3b95ee947fbc647b
naveed69 <naveed.desai.69@gmail.com>
2025-04-11 10:53:13 +0530

    completed modules

---
+++ Project-19/xcountrysearch/src/components/Display.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import styles from "./styles.module.css";
+const Display = () => {
+  const [countires, setCountries] = useState([]);
+  const [filterCountires, setFilterCountires] = useState([]);
+  let search = "";
+  let i = 0;
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const response = await fetch(
+          "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+        );
+        const responseData = await response.json();
+
+        setCountries(responseData);
+        setFilterCountires(responseData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchApi();
+  }, []);
+  const handleChange = (e) => {
+    e.preventDefault();
+    search = e.target.value;
+    setFilterCountires(
+      countires.filter((country) => {
+        return country.common.toLowerCase().includes(search.toLowerCase());
+      })
+    );
+  };
+  return (
+    <div className={styles.main}>
+      <input
+        type="text"
+        placeholder="Search for Couuntry"
+        id="search"
+        onChange={handleChange}
+      ></input>
+      <hr />
+      <div className={styles.container}>
+        {filterCountires.length > 0 ? (
+          <>
+            {filterCountires.map((country) => {
+              i++;
+              return (
+                <div key={i} className={styles.countryCard}>
+                  <img src={country.png} alt={country.comman} />
+                  <p>{country.common}</p>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Display;
