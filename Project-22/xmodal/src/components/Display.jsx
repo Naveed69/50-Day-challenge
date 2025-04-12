d18aba18a63823f0039413b88c8572c579053789
naveed69 <naveed.desai.69@gmail.com>
2025-04-12 18:42:03 +0530

    created modal need to add form

--- Project-22/xmodal/src/components/Display.jsx
+++ Project-22/xmodal/src/components/Display.jsx
@@ -11,49 +11,7 @@ const Display = () => {
       {isOpen && (
         <div onClick={() => setIsOpen(false)} className="modal show-modal">
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            {/* <form onSubmit={handlerSubmit} className="form_modal">
-              <h2>Fill Details</h2>
-              <p className="title">Username:</p>
-              <input
-                type="text"
-                className="fill"
-                id="username"
-                required
-                value={name}
-                onChange={handlerName}
-              />
-              <p className="title">Email Address:</p>
-              <input
-                type="email"
-                className="fill"
-                id="email"
-                required
-                value={email}
-                onChange={handlerEmail}
-              />
-              <p className="title">Phone Number:</p>
-              <input
-                type="tel"
-                value={no}
-                onChange={phoneHandler}
-                className="fill"
-                id="phone"
-                required
-              />
-              <p className="title">Date of Birth:</p>
-              <input
-                type="date"
-                className="fill"
-                id="dob"
-                required
-                onChange={handlerDate}
-                value={dob}
-              />
-              <br />
-              <button type="submit" className="btn submit-button">
-                Submit
-              </button>
-            </form> */}
+            <form></form>
           </div>
         </div>
       )}
