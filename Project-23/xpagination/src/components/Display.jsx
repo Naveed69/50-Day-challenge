9d8ef46876edfaacc049fa6ed38b118fe4b0f40c
naveed69 <naveed.desai.69@gmail.com>
2025-04-13 00:24:57 +0530

    facing issue with prev pagination

---
+++ Project-23/xpagination/src/components/Display.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import "./Display.css";
+const Display = () => {
+  const [tableData, setableData] = useState([]);
+  const [filterTable, setFilterTable] = useState([]);
+  const [pageno, setPageno] = useState(1);
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const response = await fetch(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        const jsonData = await response.json();
+        setableData(jsonData);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchApi();
+  }, []);
+  useEffect(() => {
+    setFilterTable(tableData.slice(0, 10));
+  }, [tableData]);
+
+  const handleNext = () => {
+    const nextpage = Math.ceil(tableData.length / 10);
+    if (pageno === nextpage) {
+      return;
+    } else {
+      let start = pageno * 10;
+      let end = start + 10;
+      setFilterTable(tableData.slice(start, end));
+      setPageno(pageno + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (pageno === 1) {
+      return;
+    } else {
+      setPageno(pageno - 1);
+      let start = pageno * 10 - 10;
+      let end = start + 10;
+      setFilterTable(tableData.slice(start, end));
+    }
+  };
+  return (
+    <>
+      <h1>Employee Data Table</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filterTable.map((employee) => {
+            return (
+              <tr key={employee.id}>
+                <td>{employee.id}</td>
+                <td>{employee.name}</td>
+                <td>{employee.email}</td>
+                <td>{employee.role}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button type="button" onClick={handlePrev}>
+        Previous
+      </button>
+      <span className="pageNo">{pageno}</span>
+      <button type="button" onClick={handleNext}>
+        Next
+      </button>
+    </>
+  );
+};
+export default Display;
