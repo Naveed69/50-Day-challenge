49f48f037322b6c8b215d933f9242fd7e989fb22
naveed69 <naveed.desai.69@gmail.com>
2025-04-13 12:49:01 +0530

    completed function

--- Project-24/xstates/src/components/Display.jsx
+++ Project-24/xstates/src/components/Display.jsx
@@ -74,7 +74,7 @@ const Display = () => {
         })}
       </select>
       <select
-        // disabled={!countryName}
+        disabled={!countryName}
         value={stateName}
         onChange={(e) => setStateName(e.target.value)}
       >
@@ -92,7 +92,7 @@ const Display = () => {
       <select
         value={cityName}
         onChange={(e) => setCityName(e.target.value)}
-        // disabled={!stateName}
+        disabled={!stateName}
       >
         <option disabled value="">
           --Select City--
