ee080b7fdb7be16950dd26b59d05c1062cfb57e6
naveed69 <naveed.desai.69@gmail.com>
2025-04-19 08:55:09 +0530

    Router app

---
+++ Project-30/routing-app/src/components/Contact.js
@@ -0,0 +1,5 @@
+function Contact() {
+  return <h2>Contact us at: contact@example.com</h2>;
+}
+
+export default Contact;
--- Project-30/routing-app/src/components/Display.jsx
+++ Project-30/routing-app/src/components/Display.jsx
@@ -1,4 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 const Display = () => {
-  return <h1>Router</h1>;
+  return (
+    <Router>
+      <div>
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/" style={{ margin: "0 10px" }}>
+            Home
+          </Link>
+          <Link to="/about" style={{ margin: "0 10px" }}>
+            About
+          </Link>
+          <Link to="/contact" style={{ margin: "0 10px" }}>
+            Contact
+          </Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        
+      </div>
+    </Router>
+  );
 };
 export default Display;
