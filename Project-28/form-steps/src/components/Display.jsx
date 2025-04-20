f2a5d071d36a63045a2910e35e9312f67ae6d9e5
naveed69 <naveed.desai.69@gmail.com>
2025-04-20 14:43:33 +0530

    form steps under progress

--- Project-28/form-steps/src/components/Display.jsx
+++ Project-28/form-steps/src/components/Display.jsx
@@ -1,4 +1,18 @@
+import Card from "./Card/Card";
+import "./Display.css";
 const Display = () => {
-  return <>Form Steps</>;
+  return (
+    <div className="container">
+      <h1>Form Steps</h1>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <div className="btn">
+        <button type="button">Previous</button>
+        <button type="button">Next</button>
+      </div>
+    </div>
+  );
 };
 export default Display;
