b44abb1cb7d0c7f4bc6191150af605ef6afca846
naveed69 <naveed.desai.69@gmail.com>
2025-04-23 12:00:56 +0530

    Added funcationality to delete button and some functions to edit

--- Project-33/new-to-do-list/src/components/Card/Card.jsx
+++ Project-33/new-to-do-list/src/components/Card/Card.jsx
@@ -1,12 +1,51 @@
+import { useState } from "react";
 import "./Card.css";
-const Card = ({ task, id }) => {
+const Card = ({ task, id, tasks, setTasks }) => {
+  const [edit, setEdit] = useState(false);
+  const [editedValue, setEditedValue] = useState("");
+  const handleEdit = () => {
+    setEdit(true);
+    setEditedValue(task.task);
+  };
+
+  const handleDelete = (task) => {
+    const updatedList = [...tasks].filter((t) => (task.id !== t.id ? t : null));
+    setTasks(updatedList);
+  };
+  const handleCancel = () => {
+    setEdit(false);
+    setEditedValue("");
+  };
   return (
-    <div className="card">
-      <div>{task}</div>
-      <span>
-        <span className="edit">&#9998;</span>
-        <span className="delete">&#x274C;</span>
-      </span>
+    <div className="card" key={id}>
+      {edit ? (
+        <input
+          type="text"
+          value={editedValue}
+          onChange={(e) => setEditedValue(e.target.value)}
+        />
+      ) : (
+        <div>{task.task}</div>
+      )}
+      {!edit ? (
+        <span>
+          <span className="edit" onClick={() => handleEdit(task)}>
+            &#9998;
+          </span>
+          <span className="delete" onClick={() => handleDelete(task)}>
+            &#x274C;
+          </span>
+        </span>
+      ) : (
+        <span>
+          <span className="edit" onClick={() => handleEdit(task)}>
+            Save
+          </span>
+          <span className="delete" onClick={handleCancel}>
+            Cancel
+          </span>
+        </span>
+      )}
     </div>
   );
 };
--- Project-33/new-to-do-list/src/components/Display.jsx
+++ Project-33/new-to-do-list/src/components/Display.jsx
@@ -3,18 +3,18 @@ import "./Display.css";
 import Card from "./Card/Card";
 const Display = () => {
   const [tasks, setTasks] = useState([
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
+    { id: 1, task: "1" },
+    { id: 2, task: "2" },
+    { id: 3, task: "3" },
+    { id: 4, task: "4" },
+    { id: 5, task: "5" },
+    { id: 6, task: "6" },
+    { id: 7, task: "7" },
+    { id: 8, task: "8" },
+    { id: 9, task: "9" },
+    { id: 10, task: "10" },
   ]);
-
+  const [ids, setIds] = useState(11);
   const [filteredTask, setFilteredTask] = useState([]);
 
   const [pageNo, setPageNo] = useState(1);
@@ -22,15 +22,16 @@ const Display = () => {
   const handleClick = (e) => {
     e.preventDefault();
     if (e.target.task.value === "") return;
-    setTasks([...tasks, e.target.task.value]);
+    setTasks([...tasks, { id: ids, task: e.target.task.value }]);
     e.target.task.value = "";
+    setIds(ids + 1);
   };
 
   useEffect(() => {
     const end = pageNo * 5;
     const start = end - 5;
     setFilteredTask(tasks.slice(start, end));
-  }, [pageNo]);
+  }, [pageNo, tasks]);
 
   const handlePrev = () => {
     setPageNo(pageNo - 1);
@@ -55,17 +56,23 @@ const Display = () => {
       </select>
       <div className="tasklist">
         {filteredTask.map((task, idx) => (
-          <Card key={idx} id={idx} task={task} />
+          <Card
+            key={idx}
+            id={idx}
+            task={task}
+            tasks={tasks}
+            setTasks={setTasks}
+          />
         ))}
       </div>
       <span className="btn">
-        <button type="button" disabled={pageNo === 1} onClick={handlePrev}>
+        <button type="button" disabled={pageNo <= 1} onClick={handlePrev}>
           Previues
         </button>
         <span className="pageno">{pageNo}</span>
         <button
           type="button"
-          disabled={pageNo === Math.ceil(tasks.length / 5)}
+          disabled={pageNo >= Math.ceil(tasks.length / 5)}
           onClick={handleNext}
         >
           Next
