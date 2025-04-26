87a633d1b39609677d267181430383d098efdea1
naveed69 <naveed.desai.69@gmail.com>
2025-04-26 13:55:11 +0530

    completed edge cases

--- Project-36/cart/src/ContexApi/Cart/CartContext.jsx
+++ Project-36/cart/src/ContexApi/Cart/CartContext.jsx
@@ -7,7 +7,7 @@ export const CartContext = createContext(null);
 export const CartProvider = (props) => {
   const [items, setItems] = useState([]);
   return (
-    <CartContext.Provider value={{ items, setItems, name: "naveed" }}>
+    <CartContext.Provider value={{ items, setItems }}>
       {props.children}
     </CartContext.Provider>
   );
