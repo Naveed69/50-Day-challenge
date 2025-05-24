8ef19e21ab6dff7f7745f6a952147a80dafd24bc
naveed69 <naveed.desai.69@gmail.com>
2025-05-24 20:47:59 +0530

    completed

--- Project-40/context-practice/src/Components/DisplayProduct/Display.jsx
+++ Project-40/context-practice/src/Components/DisplayProduct/Display.jsx
@@ -4,7 +4,7 @@ import "./Display.css";
 export const Display = () => {
   const { cart, setCart } = useCart();
   const Items = [
-    { id: 1, item: "laptop", price: 10000 },
+    { id: 1, item: "Laptop", price: 10000 },
     { id: 2, item: "Pendrive", price: 100 },
     { id: 3, item: "i-Phone", price: 2500 },
   ];
