ff7afcb5dfaacc195c07554f4679372fb543d01e
naveed69 <naveed.desai.69@gmail.com>
2025-06-06 02:01:56 +0530

    created

--- Project-41/reducer-app/src/components/Display.jsx
+++ Project-41/reducer-app/src/components/Display.jsx
@@ -1,7 +1,7 @@
 import { useReducer } from "react";
 
 export const Display = () => {
-  const [state,dispatch]=useReducer
+  const [state,dispatch]=useReducer(reduce,[])
   return (
     <>
       <h1>Display</h1>
