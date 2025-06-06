732521763ee337e0bba8866cb4c5fba862c8341b
naveed69 <naveed.desai.69@gmail.com>
2025-06-06 12:21:22 +0530

    created reducer counter

--- Project-41/reducer-app/src/components/Display.jsx
+++ Project-41/reducer-app/src/components/Display.jsx
@@ -1,10 +1,37 @@
 import { useReducer } from "react";
 
 export const Display = () => {
-  const [state,dispatch]=useReducer(reduce,[])
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "increment":
+        return { count: state.count + 1 };
+      case "decrement":
+        return { count: state.count - 1 };
+      default:
+        return state;
+    }
+  }
+
+  const increment = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrement = () => {
+    dispatch({ type: "decrement" });
+  };
   return (
     <>
-      <h1>Display</h1>
+      <h1>
+        <u>Counter</u>
+      </h1>
+      <p>Count: {state.count}</p>
+      <button type="button" onClick={increment}>
+        +
+      </button>
+      <button type="button" onClick={decrement}>
+        -
+      </button>
     </>
   );
 };
