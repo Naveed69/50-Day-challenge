0b4e8f4b65f6db99d12a479c80568da7f714d5ad
naveed69 <naveed.desai.69@gmail.com>
2025-06-07 01:39:48 +0530

    created react roject for immer

--- Project-42/redux/src/Displaying/Display.jsx
+++ Project-42/redux/src/Displaying/Display.jsx
@@ -1,18 +1,22 @@
-import { compose, pipe } from "lodash/fp";
+import { pipe } from "lodash/fp";
 export const Display = () => {
   const username = "         Naveed      ";
   const trim = (name) => name.trim();
-  const message = (name) => `Hello ${name}, welcome!`;
+  const message = (msg) => (name) => `Hello ${name}, ${msg}!`;
   const uppercase = (name) => name.toUpperCase();
 
-  const result = message(trim(uppercase(username)));
-  const composeResult = compose(message, trim, uppercase);
+  const composeResult = pipe(trim, uppercase, message("Good morning"));
   const cres = composeResult(username);
+
+  //   ----- currying-------
+  //   const add = (a) => (b) => (c) => a + b + c;
+  //   let res = add(3);
+  //   let resb = res(2);
+  //   console.log(resb(5));
 
   return (
     <>
       <h1>Redux lab</h1>
-      <p>{result}</p>
       <p>{cres}</p>
     </>
   );
