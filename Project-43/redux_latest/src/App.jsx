62db7a6a456f0383a170fb590f3dd2e529474215
naveed69 <naveed.desai.69@gmail.com>
2025-07-23 19:44:40 +0530

    created component

--- Project-43/redux_latest/src/App.jsx
+++ Project-43/redux_latest/src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
   return (
     <>
       <h1>Hello </h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <input />
     </>
   );
 }
