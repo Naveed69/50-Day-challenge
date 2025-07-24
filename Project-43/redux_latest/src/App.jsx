f072530ab130af8a2b4a56112c44613cd8be46ae
naveed69 <naveed.desai.69@gmail.com>
2025-07-24 12:28:07 +0530

    sample

--- Project-43/redux_latest/src/App.jsx
+++ Project-43/redux_latest/src/App.jsx
@@ -1,27 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import { useDispatch } from "react-redux";
-import { addNewTask } from "./store/TaskSlice";
 function App() {
-  const [input, setInput] = useState("");
-  const dispatch = useDispatch();
-  const addinput = (e) => {
-    e.preventDefault();
-    dispatch(addNewTask(input));
-    setInput("");
-  };
+
   return (
     <>
       <h1>Hello </h1>
-      <form onSubmit={addinput}>
-        <input
-          type="text"
-          name="input"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button>Add</button>
-      </form>
+    
     </>
   );
 }
--- Project-43/redux_latest/src/store/Store.jsx
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import TaskSlice from "./TaskSlice";
-export const store = configureStore({
-  reducer: TaskSlice,
-});
